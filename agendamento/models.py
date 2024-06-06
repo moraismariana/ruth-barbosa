118b641215aa1fb8db3fdcd185d3cc8a800cdc5e
moraismariana <moraismariana200@gmail.com>
2024-06-05 21:58:36 -0300

    Alteração imagens

--- agendamento/models.py
+++ agendamento/models.py
@@ -23,6 +23,11 @@ class Consulta(models.Model):
         (3, 3),
         (4, 4),
         (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
     ]
 
     OPCOES_HORARIO = [

4ac1f0875cc0e10a9bca8e12e569b784dfb69a65
moraismariana <moraismariana200@gmail.com>
2024-06-06 16:04:47 -0300

    Config Hospedagem

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ruthbarbosa-921b81c57537.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
